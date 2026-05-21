8dbdadaff9f02805ec5f61cd11af6eeaa83eb89a
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-21 11:50:43 -0400

    evite probleme de redirection

--- docs/src/service/loadUser.js
+++ docs/src/service/loadUser.js
@@ -3,17 +3,11 @@ import { supabase } from "../utils/supabaseClient.js";
 async function loadUser() {
         const { data, error } = await supabase.auth.getUser();
 
-        if (error) {
+        if (error || !data.user) {
           console.error(error);
-          document.body.innerHTML = "<h2>Erreur de connexion</h2>";
+          window.location.href = "index.html";
           return;
         }
-
-        if (!data.user) {
-          document.body.innerHTML = "<h2>Non connecté</h2>";
-          return;
-        }
-
         document.getElementById("userEmail").textContent = data.user.email;
       }
 
